29c9fa1ad06f1ae4f3f8ae7ed080e571b53b0d52
Igor Eremin <eremin.igor@icloud.com>
2024-11-19 21:25:36 -0800

    preparing tailwind

--- frontend/src/components/questions/QuestionCard.jsx
+++ frontend/src/components/questions/QuestionCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '../../components/ui/card'
 
 const QuestionCard = ({ index, question }) => {
   return (
