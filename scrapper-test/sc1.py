0a6f86a853fd8d633e5da9646cbb62a7e15070d3
Igor Eremin <eremin.igor@icloud.com>
2024-11-19 22:05:52 -0800

    in process...

--- scrapper-test/sc1.py
+++ scrapper-test/sc1.py
@@ -5,4 +5,20 @@ url = "https://apple.com"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup.title.text)
+#title
+#print(soup.title.text)
+
+#text
+#print(soup.get_text())
+
+#tags
+#print(soup.find('h'))
+
+#all sections
+section_titles = []
+
+for header in soup.find_all(['h2', 'h3']):
+    section_titles.append(header.get_text(strip=True))
+
+for title in section_titles:
+    print(title)
